797b82d0cfd108fa1a0eb1a578a71d1dc1202995
Aleris Roman <aleris.roman@onshoreoutsourcing.com>
2014-08-29 16:06:04 -0400

    Finished intial DAO

--- DAL/DAO.cs
+++ DAL/DAO.cs
@@ -8,5 +8,23 @@ namespace DAL
 {
     public class DAO
     {
+        private string Name { get; set; }
+        private int Id { get; set; }
+        public int Age { get; set; }
+        public string Address { get; set; }
+
+        public string GetName()
+        {
+            return Name;
+        }
+        public int GetId ()
+        {
+            return Id;
+        }
+        public int IncrementAge(int view)
+        {
+            return view + Age;
+        }
+
     }
 }
